ba94cff0bd77c047ed26d2e26c1e2350113abc71
Mitch <mcuthbe@gmail.com>
2024-01-07 16:38:48 +0200

    Basic get and set as text from clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use clipboard_win::{formats, get_clipboard, set_clipboard};
 
+fn get() -> String {
+    let text = get_clipboard(formats::Unicode).expect("Get clipboard contents");
+    text
 }
+
+fn set(text: &str) {
+    set_clipboard(formats::Unicode, text).expect("Set clipboard contents");
+}
+
+fn main() {}
