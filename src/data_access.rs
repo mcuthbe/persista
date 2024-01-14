6dd2cdf829cfbd43642cdb64f794b54e6e4f0e54
Mitch <mcuthbe@gmail.com>
2024-01-14 13:24:50 +0200

    Passing unit test, proper persy instantiation
    - Fixed unit test
    - Moved index creation to persy instantiation
    - db functions get persy instance so they don't always use prod
    - Because ofht eabove I can now remove db name consts

--- src/data_access.rs
+++ src/data_access.rs
@@ -6,27 +6,13 @@ use crate::{structs::Clip, enums::ClipboardItem};
 
 const CLIPS: &str = "clips";
 const INDEX_NAME: &str = "name_index";
-const DB_NAME: &str = "target/data.persy";
 
-pub fn save_clip(item: &Clip) -> Result<PersyId, Box<dyn Error>> {
-    let persy = open_database(DB_NAME)?;
-
+pub fn save_clip(persy: &Persy, item: &Clip) -> Result<PersyId, Box<dyn Error>> {
     let mut transaction = persy.begin()?;
 
-    if !transaction.exists_segment(CLIPS)? {
-        transaction.create_segment(CLIPS)?;
-    }
-
     let clip_bytes = bincode::serialize(&item)?;
-
     let result = transaction.insert(CLIPS, &clip_bytes)?;
-
-    if !transaction.exists_index(INDEX_NAME)? {
-        transaction.create_index::<String, PersyId>(INDEX_NAME, ValueMode::Cluster)?;
-    }
-
     let persy_id_string = result.to_string();
-
     transaction.put(INDEX_NAME, item.name.to_string(), persy_id_string)?;
 
     let prepared = transaction.prepare()?;
@@ -35,9 +21,7 @@ pub fn save_clip(item: &Clip) -> Result<PersyId, Box<dyn Error>> {
     Ok(result)
 }
 
-pub fn get_clip(name: &String) -> Result<Option<ClipboardItem>, Box<dyn Error>> {
-    let persy = open_database(DB_NAME)?;
-
+pub fn get_clip(persy: &Persy, name: &String) -> Result<Option<ClipboardItem>, Box<dyn Error>> {
     let id_string: Option<String> = persy.one(INDEX_NAME, name)?;
     if let Some(id_string) = id_string {
         let persy_id = &PersyId::from_str(&id_string)?;
@@ -54,11 +38,13 @@ pub fn get_clip(name: &String) -> Result<Option<ClipboardItem>, Box<dyn Error>> 
     Ok(None)
 }
 
-fn open_database(db_name: &str) -> Result<Persy, Box<dyn Error>> {
+pub fn open_database(db_name: &str) -> Result<Persy, Box<dyn Error>> {
     let persy = Persy::open_or_create_with(db_name, Config::new(), |persy| {
         let mut transaction = persy.begin()?;
 
         transaction.create_segment(CLIPS)?;
+
+        transaction.create_index::<String, String>(INDEX_NAME, ValueMode::Cluster)?;
 
         let prepared = transaction.prepare()?;
         prepared.commit()?;
@@ -66,6 +52,19 @@ fn open_database(db_name: &str) -> Result<Persy, Box<dyn Error>> {
         println!("Clips segment and Index successfully created");
         Ok(())
     })?;
+
+    let mut transaction = persy.begin()?;
+
+    if !transaction.exists_segment(CLIPS)? {
+        transaction.create_segment(CLIPS)?;
+    }
+
+    if !transaction.exists_index(INDEX_NAME)? {
+        transaction.create_index::<String, String>(INDEX_NAME, ValueMode::Cluster)?;
+    }
+
+    let prepared = transaction.prepare()?;
+    prepared.commit()?;
 
     Ok(persy)
 }
@@ -77,24 +76,22 @@ mod tests {
     use crate::util::generate_unique_value;
 
     use super::*;
-    const TEST_DB_NAME: &str = "target/test.persy";
 
     #[test]
     fn save_clip_succeeds(){
         let unique_name = generate_unique_value();
-        let persy = open_database(TEST_DB_NAME).unwrap();
 
         let clip = Clip {
-            name: unique_name.to_string(),
+            name: (&unique_name).to_string(),
             value: ClipboardItem::Text("Test".to_string()),
         };
 
+        let persy = open_database("target/test.persy").unwrap();
 
-        let result = save_clip(&clip).unwrap();
+        let result = save_clip(&persy, &clip).unwrap();
+        let persy_id_string = &PersyId::from_str(&result.to_string()).unwrap();
 
-        let clip = get_clip(&unique_name).unwrap();
-
-        let clip_data = persy.read(CLIPS, &result).unwrap().unwrap();
+        let clip_data = persy.read(CLIPS, persy_id_string).unwrap().unwrap();
         let deserialized = bincode::deserialize::<Clip>(&clip_data).unwrap();
         assert_eq!(deserialized.name, unique_name);
     }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod structs;
 mod util;
 
 use clipboard::clip_get;
-use data_access::{get_clip, save_clip};
+use data_access::{get_clip, save_clip, open_database};
 use enums::ClipboardItem;
 use structs::Clip;
 
@@ -19,8 +19,9 @@ fn main() {
         name: "Test".to_string(),
         value: ClipboardItem::Text("Test".to_string()),
     };
-    let _ = save_clip(&test_clip);
-    let values = get_clip(&"Test".to_string()).unwrap();
+    let persy = open_database("target/data.persy").unwrap();
+    let _ = save_clip(&persy, &test_clip);
+    let values = get_clip(&persy, &"Test".to_string()).unwrap();
     match values {
         Some(clip) => println!("Value: {}", clip.as_str()),
         None => println!("No value found"),
